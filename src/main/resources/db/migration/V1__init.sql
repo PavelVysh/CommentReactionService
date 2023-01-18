47a03054c7e1df9e8a38bb2bdcd9dbbd21bf1184
pvyshnevetskyi <pvyshnevetskyi@griddynamics.com>
2023-01-18 15:05:41 +0200

    database schema database migration with flyway

---
+++ src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,30 @@
+CREATE TABLE IF NOT EXISTS comments(
+id INTEGER PRIMARY KEY,
+post_id INTEGER NOT NULL,
+user_id INTEGER NOT NULL,
+created_at TIMESTAMP,
+text VARCHAR(500) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS replies(
+id INTEGER PRIMARY KEY,
+comment_id INTEGER NOT NULL,
+user_id INTEGER NOT NULL,
+created_at TIMESTAMP,
+text VARCHAR(500) NOT NULL,
+FOREIGN KEY (comment_id) REFERENCES comments(id)
+);
+
+CREATE TABLE IF NOT EXISTS likes(
+id INTEGER PRIMARY KEY,
+user_id INTEGER,
+entity_id INTEGER,
+entity_type ENUM("comment", "reply", "post", "repost")
+);
+
+CREATE TABLE IF NOT EXISTS dislikes(
+id INTEGER PRIMARY KEY,
+user_id INTEGER,
+entity_id INTEGER,
+entity_type ENUM("comment", "reply", "post", "repost")
+);
